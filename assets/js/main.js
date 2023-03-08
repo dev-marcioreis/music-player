30e197d992391ad2616e0e8b2be67c982cc63eeb
Marcio Reis <dev.marcioreis@gmail.com>
2023-03-08 16:20:35 -0300

    Duração da musica js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ let mainAudio = wrapper.querySelector('#audio')
 let playPouseBtn = wrapper.querySelector('.play__pause')
 let prevBtn = wrapper.querySelector('#prev-song')
 let nextBtn = wrapper.querySelector('#next-song')
+let progressBar = wrapper.querySelector('.progress__bar')
 
 
 let musicIndex = 1
@@ -60,3 +61,39 @@ nextBtn.addEventListener('click', () => {
 prevBtn.addEventListener('click', () => {
     prevMusic()
 })
+
+mainAudio.addEventListener('timeupdate', e => {
+    const currentTime = e.target.currentTime
+    const duration = e.target.duration
+
+    let progressWidth = (currentTime / duration) * 100
+
+    progressBar.style.width = `${progressWidth}%`
+
+    let musicCurrentTime = wrapper.querySelector('.current')
+    let musicDurationTime = wrapper.querySelector('.current__duration')
+
+    mainAudio.addEventListener('loadeddata', () => {
+
+        let audioDuration = mainAudio.duration
+        let totalMinutes = Math.floor(audioDuration / 60)
+        let totalSeconds = Math.floor(audioDuration % 60)
+
+        if(totalSeconds < 10) {
+            totalSeconds = `0${totalSeconds}`
+        }
+
+        musicDurationTime.innerText = `${totalMinutes} : ${totalSeconds}`
+    })
+
+        let currentMinutes = Math.floor(currentTime / 60)
+        let currentSeconds = Math.floor(currentTime % 60)
+
+        if(currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`
+        }
+
+        musicCurrentTime.innerText = `${currentMinutes} : ${currentSeconds}`
+
+})
+
